4276ec3b1ded54f65baccb32e53732f601585786
Tanhya <Tanhya@users.noreply.github.com>
2020-01-18 21:38:28 -0700

    Add files via upload

---
+++ HD_Project.R
@@ -0,0 +1,8 @@
+hunter_douglas_50k <- read.csv("~/Desktop/Spring Term/Project Course/Hunter Douglas Quality Data 50K.csv")
+hunter_douglas_main <- read.csv("~/Desktop/Spring Term/Project Course/Hunter Douglas Quality Data.csv")
+
+table(hunter_douglas_main$PRODUCT_CATEGORY, hunter_douglas_main$ORIGINAL_PLANT)
+
+hunter_sil <- hunter_douglas_main[hunter_douglas_main$PRODUCT_CATEGORY == '02 Silhouette/Nantucket' & hunter_douglas_main$ORIGINAL_PLANT == 'G', ]
+summary(hunter_sil)
+
